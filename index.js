179d88955957886c223a168fa46c6287679e8678
Abdallah Safar <abdallah.safar01@gmail.com>
2021-04-27 15:02:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+  return "This Saturday, I want to " + activity + "!";
+}
+
+function mondayWork(activity = "go to the office") {
+    return "This Monday, I will " + activity + ".";
+}
+
+function wrapAdjective(adj = "*") {
+  return (param = "special") => {
+    return "You are " + adj + param + adj + "!";
+  }
+}
+
+let Calculator = {
+  add: () => 1+3;
+  substract: () => 1-3;
+  multiply: () => 1*3;
+  divide: () => 10/5;
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+
+console.log(encouragingPromptFunction("great"));
